4cb851973d4daef04ac6cf91d2c8be5ae10ba63e
David Pfeffer <bytenik@bytenik.com>
2014-09-10 11:37:51 -0400

    removed rtools exe in favor of download on boxes

--- Startup/install_debugger.cmd
+++ Startup/install_debugger.cmd
@@ -1,6 +1,7 @@
 if "%ComputeEmulatorRunning%"=="true" goto :EOF
 
-start /wait Startup\rtools_setup_x64.exe /install /passive /norestart
+powershell wget https://www.dropbox.com/s/k4k0xsubgqr2g5x/rtools_setup_x64.exe?dl=1 -OutFile rtools_setup_x64.exe
+start /wait rtools_setup_x64.exe /install /passive /norestart
 start /wait "Remote Debugger Computer Prep" "%PROGRAMFILES%\Microsoft Visual Studio 12.0\Common7\IDE\Remote Debugger\x64\msvsmon.exe" /prepcomputer /domain /private /public /quiet
 start "Remote Debugger" "%PROGRAMFILES%\Microsoft Visual Studio 12.0\Common7\IDE\Remote Debugger\x64\msvsmon.exe" /noauth /anyuser /nosecuritywarn /silent /timeout 2147483646
 
